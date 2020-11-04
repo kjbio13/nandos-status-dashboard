5684aa66e95b9a14edb02af7473068a8dfca0f1f
Kevin Souribio (Personal) <kevinsouribio@nandos-ltmb-kev.local>
2020-11-04 15:37:10 +0000

    m

--- client/src/components/item-card/item-card.component.jsx
+++ client/src/components/item-card/item-card.component.jsx
@@ -26,11 +26,7 @@ const ItemCard = ({ props }) => {
             {props.imageUrl}<br/>
             {props.subtext_1}<br/><br/><br/>
             {status.status}<br/>
-<<<<<<< HEAD
-            {status.statusText}<br/>Does this change?<br/>
-=======
             {status.statusText}<br/><br/><br/>
->>>>>>> a69fa2e45622f70ddaf0d19d61e27b3c9ca1d8cd
             {/* {console.log(props.id)}<br/> */}
         </>
     )
